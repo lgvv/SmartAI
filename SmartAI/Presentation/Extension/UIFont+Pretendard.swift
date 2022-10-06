5598c120fc45f9b18a4f412ce2ec6fd3eefaed0e
이건우 <lgw9898@naver.com>
2022-10-06 18:50:00 +0900

    [추가] Pretendard 폰트와 UIFont 확장 추가
    번들에 등록되지 않은 스타일을 요청하면 디버그 빌드에서 즉시 드러나도록
    assertionFailure로 알린 뒤 시스템 폰트로 대체한다.

---
+++ SmartAI/Presentation/Extension/UIFont+Pretendard.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension UIFont {
+    enum Pretendard: String {
+        case light = "Pretendard-Light"
+        case regular = "Pretendard-Regular"
+        case medium = "Pretendard-Medium"
+        case semiBold = "Pretendard-SemiBold"
+        case bold = "Pretendard-Bold"
+        case extraBold = "Pretendard-ExtraBold"
+    }
+
+    static func pretendard(_ style: Pretendard, size: CGFloat) -> UIFont {
+        guard let font = UIFont(name: style.rawValue, size: size) else {
+            assertionFailure("\(style.rawValue) 폰트가 번들에 등록되지 않았습니다")
+            return .systemFont(ofSize: size)
+        }
+
+        return font
+    }
+}
