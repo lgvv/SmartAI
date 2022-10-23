574c4afe323213027ac04561f73ff24f6e20c76d
이건우 <lgw9898@naver.com>
2022-10-23 18:10:00 +0900

    [추가] 이미지 방향 변환 확장 추가
    도메인 ImageOrientation과 UIKit CoreGraphics 방향을 상호 변환한다.

---
+++ SmartAI/Data/Extension/ImageOrientation+UIKit.swift
@@ -0,0 +1,33 @@
+import ImageIO
+import UIKit
+
+extension ImageOrientation {
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default: self = .up
+        }
+    }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: ImageOrientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
